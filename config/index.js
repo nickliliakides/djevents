1c524613777418d01811d9f9e48647247ab6f520
Nick Liliakides <nick.liliakides@gmail.com>
2024-05-07 05:24:04 +0100

    test api var

--- config/index.js
+++ config/index.js
@@ -1,5 +1,6 @@
 export const API_URL =
-	process.env.NEXT_PUBLIC_API_URL || 'http://localhost:1337/api'
+	process.env.NEXT_PUBLIC_API_URL ||
+	'https://djevents-strapi-be.onrender.com/api'
 
 export const NEXT_URL =
 	process.env.NEXT_PUBLIC_FRONTEND_URL || 'http://localhost:3000'
