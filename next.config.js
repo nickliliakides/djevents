a7481cc33d93cf1725c1f3e105495a48d02f9229
Nick Liliakides <nick.liliakides@gmail.com>
2024-05-07 01:35:19 +0100

    eslint config 4

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 module.exports = {
-  reactStrictMode: true,
-  images: {
-    domains: ['res.cloudinary.com']
-  }
+	reactStrictMode: true,
+	images: {
+		domains: ['res.cloudinary.com'],
+	},
+	webpack5: false,
+	eslint: {
+		dirs: ['pages'], // Only run ESLint on the 'pages' and 'utils' directories during production builds (next build)
+	},
 }
