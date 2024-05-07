aa1855e55db8e8e984b2b638c7eefcbc64f76d4d
Nick Liliakides <nick.liliakides@gmail.com>
2024-05-07 04:07:57 +0100

    data.json update imports

--- pages/api/events/[slug].js
+++ pages/api/events/[slug].js
@@ -1,11 +1,11 @@
-const { events } = require('./data.json')
+const { events } = require('../../../data.json')
 
 export default function handler(req, res) {
-  const evt = events.filter(ev => ev.slug === req.query.slug)
+	const evt = events.filter(ev => ev.slug === req.query.slug)
 
-  if(req.method === 'GET') res.status(200).json(evt)
-  else {
-    res.setHeader('Allow', ['GET'])
-    res.status(405).json({ message: `Method ${req.method} is not allowed`})
-  }
+	if (req.method === 'GET') res.status(200).json(evt)
+	else {
+		res.setHeader('Allow', ['GET'])
+		res.status(405).json({ message: `Method ${req.method} is not allowed` })
+	}
 }
