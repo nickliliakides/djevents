74555cf5ff14cb4257474585b0528fd6b34c40ee
Nick Liliakides <nick.liliakides@gmail.com>
2024-05-09 12:07:21 +0100

    geocode api change back to mapbox

--- components/EventMap.jsx
+++ components/EventMap.jsx
@@ -17,16 +17,14 @@ const EventMap = ({ evt }) => {
 
 	useEffect(() => {
 		fetch(
-			`https://geocode.search.hereapi.com/v1/
-			geocode
-			?q=${evt.address}&apiKey=${process.env.NEXT_PUBLIC_HERE_API_KEY}`
+			`https://api.geoapify.com/v1/geocode/search?text=${evt.address}&apiKey=${process.env.NEXT_PUBLIC_GEOAPIFY_API_KEY}`
 		)
 			.then(response => response.json())
 			.then(result => {
-				const { lat, lng } = result.items[0].position
+				const { lat, lon } = result.features[0].properties
 				setLat(lat)
-				setLng(lng)
-				setViewport({ ...viewport, latitude: lat, longitude: lng })
+				setLng(lon)
+				setViewport({ ...viewport, latitude: lat, longitude: lon })
 				setLoading(false)
 			})
 			.catch(error => console.log('error', error))
